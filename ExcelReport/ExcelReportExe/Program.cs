43580f941e943c86f09ba8e3cdb476d851ce122b
ssaattww <taiga202500@gmail.com>
2025-12-30 16:15:31 +0900

    feat: Add initial implementation of Program.cs to parse DSL definition from XML file

---
+++ ExcelReport/ExcelReportExe/Program.cs
@@ -0,0 +1,5 @@
+﻿using ExcelReportLib.DSL;
+using ExcelReportLib.DSL.AST;
+
+using System;
+DslParser.ParseFromFile(@"C:\Users\taiga\source\repos\ExcelReport\ExcelReport\ExcelReportLibTest\TestDsl\DslDefinition_FullTemplate_Sample_v1.xml");
